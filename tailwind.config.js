ca0d06dc83e50cff3cd6799b0d4e5967b6a622ff
offiahchiaz <offiahchiaz@gmail.com>
2021-06-12 14:56:38 +0100

    Layout for nav done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#12141d",
+        secondary: "#13b5ea",
+        btncolor: "#e6027e"
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
   },
   variants: {
     extend: {},
